1d1c695f2f290af17a74a538571836de1432e843
mazu20 <maciek789@gmail.com>
2019-02-17 13:12:52 +0000

    Initial commit

---
+++ Algorithms/MainViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewCell.swift
+//  Algorithms
+//
+//  Created by Maciej Kowalski on 17/02/2019.
+//  Copyright © 2019 Maciej Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class MainViewCell: UICollectionViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        //contentView.backgroundColor = .white
+    }
+}
---
+++ Algorithms/MainViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MainViewController.swift
+//  Algorithms
+//
+//  Created by Maciej Kowalski on 17/02/2019.
+//  Copyright © 2019 Maciej Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UICollectionViewController {
+    
+    fileprivate let mainViewCellReuseIdentifier = "MainViewCell"
+    fileprivate let itemsPerRow: CGFloat = 2
+    fileprivate let sectionInsets = UIEdgeInsets(top: 20, left: 16, bottom: 20, right: 16)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MainViewController loaded")
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: mainViewCellReuseIdentifier, for: indexPath) as! MainViewCell
+        
+        cell.layer.cornerRadius = 8
+        cell.clipsToBounds = true
+        switch indexPath.row {
+        case 0:
+            cell.backgroundColor = .red
+        case 1:
+            cell.backgroundColor = .green
+        case 2:
+            cell.backgroundColor = .blue
+        case 3:
+            cell.backgroundColor = .orange
+        default:
+            cell.backgroundColor = .purple
+        }
+        
+        return cell
+    }
+    
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
+
