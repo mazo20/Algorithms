641ca27ba79807992118192f3bdab3e2ee8823c0
mazu20 <maciek789@gmail.com>
2019-02-17 12:52:17 +0000

    Initial Commit

---
+++ Algorithms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Algorithms
+//
+//  Created by Maciej Kowalski on 17/02/2019.
+//  Copyright © 2019 Maciej Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
